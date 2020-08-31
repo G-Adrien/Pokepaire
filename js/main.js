4bb96bb4917862714dba83f38d319bbb4b276cea
Adrien Girin <girin.adrien.pro@gmail.com>
2020-08-31 17:13:18 +0200

    start js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let cards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
+
+let gameZone = document.getElementById("gameZone");
+
+for (card of cards){
+    gameZone.innerHTML = "<div>test""</div>";
+    console.log("ok");
+}
